4b8dc0a8a7371f388c565355a1232aec15a4a630
cecile.hirschauer <cecile.hirschauer@suricats-consulting.com>
2022-01-19 17:54:28 +0100

    Slider fonctionnalities

--- src/Components/Slider/Slider.js
+++ src/Components/Slider/Slider.js
@@ -1,41 +1,58 @@
-import React, {useState} from 'react';
-import './Slider.css';
-import dataSlider from './dataSlider';
-import BtnSlider from './BtnSlider';
-
-
+import React, { useState } from "react";
+import "./Slider.css";
+import dataSlider from "./dataSlider";
+import BtnSlider from "./BtnSlider";
 
 const Slider = () => {
-    const [slideAnim, setSlideAnim] = useState({
+  const [slideAnim, setSlideAnim] = useState({
+    index: 1,
+    inProgress: false,
+  });
+
+  const nextSlide = () => {
+    if (slideAnim.index !== dataSlider.length) {
+      setSlideAnim({
+        index: slideAnim + 1,
+        inProgress: true,
+      });
+    } else if (slideAnim.index === dataSlider.length) {
+      setSlideAnim({
         index: 1,
-        inProgress: false
-    })
-
-    const nextSlide = () => {
-        console.log("NEXT");
+        inProgress: true,
+      });
     }
+  };
 
-    const prevSlide = () => {
-        console.log("PREV");
-    }
-    return (
-    <div className='container-slider'>
-       {dataSlider.map((obj, index) => {
-                return (
-                    <div
-                    key={obj.id}
-                    className={slideAnim.index === index + 1 ? "slide active-anim" : "slide"}
-                    >
-                        <img src={process.env.PUBLIC_URL + `/Imgs/img${index + 1}.jpg`} alt="" />
-                    </div>
-                )
-            })}
-            <BtnSlider moveSlide={nextSlide} direction={"next"}/>
-            <BtnSlider moveSlide={prevSlide} direction={"prev"}/>
+  const prevSlide = () => {
+    if (slideAnim.index !== 1) {
+        setSlideAnim({
+          index: slideAnim - 1,
+          inProgress: true,
+        });
+      } else if (slideAnim.index === 1) {
+        setSlideAnim({
+          index: 5,
+          inProgress: true,
+        });
+      }
+  };
 
-    </div>);
-}
-
-
+  return (
+    <div className="container-slider">
+      {dataSlider.map((obj, index) => {
+        return (
+            <div
+            key={obj.id}
+            className={slideAnim.index === index + 1 ? "slide active-anim" : "slide"}
+            >
+                <img src={process.env.PUBLIC_URL + `/Imgs/img${index+1}.jpg`} alt="" />
+            </div>
+        );
+      })}
+      <BtnSlider moveSlide={nextSlide} direction={"next"} />
+      <BtnSlider moveSlide={prevSlide} direction={"prev"} />
+    </div>
+  );
+};
 
 export default Slider;
