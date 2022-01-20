5ee19388b231b09ca27d1b39c46c04ac34af5c05
cecile.hirschauer <cecile.hirschauer@suricats-consulting.com>
2022-01-20 10:30:21 +0100

    Prevent spam click

--- src/Components/Slider/Slider.js
+++ src/Components/Slider/Slider.js
@@ -3,56 +3,64 @@ import "./Slider.css";
 import dataSlider from "./dataSlider";
 import BtnSlider from "./BtnSlider";
 
-const Slider = () => {
+export default function Slider() {
   const [slideAnim, setSlideAnim] = useState({
     index: 1,
     inProgress: false,
   });
 
   const nextSlide = () => {
-    if (slideAnim.index !== dataSlider.length) {
-      setSlideAnim({
-        index: slideAnim + 1,
-        inProgress: true,
-      });
-    } else if (slideAnim.index === dataSlider.length) {
-      setSlideAnim({
-        index: 1,
-        inProgress: true,
-      });
+    if (slideAnim.index !== dataSlider.length && !slideAnim.inProgress) {
+      setSlideAnim({ index: slideAnim.index + 1, inProgress: true });
+
+      setTimeout(() => {
+        setSlideAnim({ index: slideAnim.index + 1, inProgress: false });
+      }, 400);
+    } else if (slideAnim.index === dataSlider.length && !slideAnim.inProgress) {
+      setSlideAnim({ index: 1, inProgress: true });
+
+      setTimeout(() => {
+        setSlideAnim({ index: 1, inProgress: false });
+      }, 400);
     }
   };
 
   const prevSlide = () => {
-    if (slideAnim.index !== 1) {
-        setSlideAnim({
-          index: slideAnim - 1,
-          inProgress: true,
-        });
-      } else if (slideAnim.index === 1) {
-        setSlideAnim({
-          index: 5,
-          inProgress: true,
-        });
-      }
+    if (slideAnim.index !== 1 && !slideAnim.inProgress) {
+      setSlideAnim({ index: slideAnim.index - 1, inProgress: true });
+
+      setTimeout(() => {
+        setSlideAnim({ index: slideAnim.index - 1, inProgress: false });
+      }, 400);
+    } else if (slideAnim.index === 1 && !slideAnim.inProgress) {
+      setSlideAnim({ index: 5, inProgress: true });
+
+      setTimeout(() => {
+        setSlideAnim({ index: 5, inProgress: false });
+      }, 400);
+    }
   };
 
+ 
   return (
     <div className="container-slider">
       {dataSlider.map((obj, index) => {
         return (
-            <div
+          <div
             key={obj.id}
-            className={slideAnim.index === index + 1 ? "slide active-anim" : "slide"}
-            >
-                <img src={process.env.PUBLIC_URL + `/Imgs/img${index+1}.jpg`} alt="" />
-            </div>
+            className={
+              slideAnim.index === index + 1 ? "slide active-anim" : "slide"
+            }
+          >
+            <img
+              src={process.env.PUBLIC_URL + `/Imgs/img${index + 1}.jpg`}
+              alt=""
+            />
+          </div>
         );
       })}
       <BtnSlider moveSlide={nextSlide} direction={"next"} />
       <BtnSlider moveSlide={prevSlide} direction={"prev"} />
     </div>
   );
-};
-
-export default Slider;
+}
